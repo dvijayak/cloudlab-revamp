951cdca6ce9c413797ae329da96d84be7079b412
Daniel <dvijayak@uoguelph.ca>
2012-12-12 04:00:10 -0500

    Just before refactoring js session validation

--- js/util.js
+++ js/util.js
@@ -1,5 +1,30 @@
 function Util () {
+    
+    console.log("Created the Util object");
 
+    /* Fields */
+    
+    // URIs/URLs to important internal and external resources    
+    this.paths = new Array();
+    this.paths.protocol = "http://";
+    this.paths.domain = "localhost";
+    this.paths.root = "/cloudlab_revamp/";
+    this.paths.img = "img/";
+    this.paths.js = "js/";
+    this.paths.php = "php/";
+    
+    this.urls = new Array();    
+    this.urls.server = this.paths.protocol + this.paths.domain + this.paths.root;
+    this.urls.php = new Array();
+    this.urls.php.main = this.urls.server + this.paths.php + "main.php";
+    
+    // Pre-configured AJAX settings
+    this.ajax = new Array();
+    this.ajax.url = this.urls.php.main;
+    this.ajax.dataType = "json";    
+
+    /* Methods */
+    
     this.cookiesToArray = function () {
         var array = new Array();
         // key/value pairs in document.cookie are delimited by "; " instead of ";"
@@ -10,6 +35,33 @@ function Util () {
             array[cookie[0]] = cookie[1];
         }							
         return array;
+    }
+    
+    // Validates the session and then optionally performs a given AJAX task (with optional input data) to the same server
+    this.validateSession = function (task, data) {        
+        $.post(this.ajax.url,
+               function (output) {
+                    // Ensure that the user is logged in
+                    if (output.status == "OK") {                    
+                        var cookies = this.cookiesToArray();
+                        
+                        // Performs the AJAX task only if it was specified
+                        if (task !== undefined) {
+                            if (data !== undefined) {
+                                $.post(this.ajax.url, data, task, this.ajax.dataType);   
+                            }
+                            else {
+                                $.post(this.ajax.url, task, this.ajax.dataType);
+                            }
+                        }                    
+                    }
+                    // Incorrect login credentials
+                    else if (output.status == "INTRUDER") {
+                        alert("Back off, intruder!");
+                        window.location.href = "index.html";							
+                    }
+                },
+               this.ajax.dataType);
     }
     
 }
