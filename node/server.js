8343925e97855081c4d33ff42144e1bcdf9ad8e7
Daniel <dvijayak@uoguelph.ca>
2012-12-13 21:26:47 -0500

    Finally got ace editor to work.

--- node/server.js
+++ node/server.js
@@ -2,12 +2,7 @@
 
 var app = require('http').createServer(handler),
 	io = require('socket.io').listen(app),
-	exec = require('child_process').exec,
-	nstatic = require('node-static'); // for serving files
-
-// This will make all the files in the current folder
-// accessible from the web
-var fileServer = new  nstatic.Server('./');
+	exec = require('child_process').exec;	
 
 // This is the port for our web server.
 // you will need to go to http://localhost:8080 to see it
